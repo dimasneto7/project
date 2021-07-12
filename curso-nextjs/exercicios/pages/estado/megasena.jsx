c307095867475049a91d35742b2004535128ba65
Dimas Neto <dimas_fcn@outlook.com>
2021-07-12 13:10:20 -0300

    exercicio

---
+++ curso-nextjs/exercicios/pages/estado/megasena.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { mega } from "../../functions/mega";
+import NumerosDisplay from "../../components/NumeroDisplay";
+
+export default function megasena() {
+  const [qtde, setQtde] = useState(6);
+  const [numeros, setNumeros] = useState([]);
+
+  useEffect(() => {
+    setNumeros(mega());
+  }, []);
+
+  function renderizarNumeros() {
+    return numeros.map((numero) => (
+      <NumerosDisplay key={numero} numero={numero} />
+    ));
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1>Mega Sena</h1>
+      <div
+        style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      >
+        {renderizarNumeros()}
+      </div>
+      <div>
+        <input
+          type="number"
+          min={6}
+          max={20}
+          value={qtde}
+          onChange={(ev) => setQtde(ev.target.value)}
+        />
+        <button onClick={() => setNumeros(mega(qtde))}>Gerar Aposta</button>
+      </div>
+    </div>
+  );
+}
