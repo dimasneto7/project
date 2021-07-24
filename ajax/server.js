9d82633fc80003a116ba0aa88e792476cb9054f8
Dimas Neto <dimas_fcn@outlook.com>
2021-07-24 16:08:28 -0300

    ajax

--- ajax/server.js
+++ ajax/server.js
@@ -29,6 +29,20 @@ app.post("/upload", (req, res) => {
   });
 });
 
+app.post("/formulario", (req, res) => {
+  res.send({
+    ...req.body,
+    id: 1,
+  });
+});
+
+app.get("/parOuImpar", (req, res) => {
+  const par = parseInt(req.query.numero) % 2 === 0;
+  res.send({
+    resultado: par ? "par" : "impar",
+  });
+});
+
 app.listen(8080, () => console.log("Executando..."));
 
 // responsável por fazer o body do parser da requizição
