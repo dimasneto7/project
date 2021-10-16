1e5663304d3f0c0ad67bb48028a9f5822ea17048
Dimas Neto <dimas_fcn@outlook.com>
2021-10-16 13:36:35 -0300

    async

--- async/index-2.js
+++
@@ -1,7 +0,0 @@
-setTimeout(() => {
-  console.log("setTimeout");
-}, 1);
-
-for (let i = 0; i <= 10000; i++) {
-  console.log(i);
-}
--- async/index-4.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios");
-const gitHubApi = "https://api.github.com/users/dimasneto7/repos";
-
-axios.get(gitHubApi).then((res) => {
-  console.log(res.data);
-});
-console.log("teste");
